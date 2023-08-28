4da88588e5c9a0ca89a9785a97047b967e355c63
CardinalCandle <youssef-kj@outlook.fr>
2023-08-28 03:00:27 +0100

    modifed settings.py file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,22 +126,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-
-]
-
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-]
 
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000',
-     'http://127.0.0.1:3000',
-     'http://localhost:8000',
-     'http://127.0.0.1:8000',
 ]
